d6bb1c01e5841cfe186b3ed579f9cb7d422a0f3e
!vRP.Koala.RazorX <76109593+OliveiraBRE@users.noreply.github.com>
2021-06-08 09:33:17 -0300

    Add files via upload

---
+++ project01/assets/js/script.js
@@ -0,0 +1,15 @@
+// trocar a setagem das classes
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses(){
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
